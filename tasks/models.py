33aae39a9483117773b29fe1b253029f9c414af0
spkuehl <skuehlhorn@gmail.com>
2020-05-22 13:58:09 -0500

    Added initial task and keyword models with migrations.

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=20)
+    user_submitted = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=99)
+    user_submitted = models.ForeignKey(User, on_delete=models.CASCADE)
+    keywords = models.ManyToManyField(Keyword, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    accepted = models.BooleanField(default=False)
+    retired = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
